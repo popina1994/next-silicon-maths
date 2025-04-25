5cd306532d99721116168a9c03ca58b51bd93b7d
popina1994 <djordjezivanovicva@gmail.com>
2025-04-25 11:34:34 +0100

    1. Adding proper fuzzy harnessing.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mkdir -p build
 cd build
-rm * -rf
+#rm * -rf
 
 #C_COMPILER=/usr/bin/gcc
 #CXX_COMPILER=/usr/bin/g++
@@ -12,6 +12,8 @@ CXX_COMPILER=/usr/bin/clang++
 AFL_C_COMPILER=/usr/bin/afl-clang
 AFL_CXX_COMPILER=/usr/bin/afl-clang++
 
+LIB_FUZZ_NUM_RUNS=10
+
 cmake .. -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER\
     -DCMAKE_AFL_C_COMPILER=$AFL_C_COMPILER -DCMAKE_AFL_CXX_COMPILER=$AFL_CXX_COMPILER
 make
@@ -21,8 +23,8 @@ make
 mkdir -p input
 mkdir -p corpus
 echo hello > corpus/seed.txt
-echo "HOHO"
-./Next-Silicon-Maths-fuzz corpus/
+LIB_FUZZ_NUM_RUNS=10
+./Next-Silicon-Maths-fuzz corpus/ -runs=${LIB_FUZZ_NUM_RUNS}
 
 export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1
 #afl-fuzz -i input -o output -- ./Next-Silicon-Maths-afl
--- test/fuzzy/main_fuzzy.cpp
+++ test/fuzzy/main_fuzzy.cpp
@@ -1,76 +1,18 @@
-// #include <cmath>
-// #include <cstdint>
-// #include <cstring>
-// #include <iostream>
-// #include <iomanip>
-// #include "math_custom_fun.h"
-
-// uint8_t charToNum(uint8_t num)
-// {
-//     if (num >= '0' and num <= '9')
-//     {
-//         return num - '0';
-//     }
-//     else if (num >= 'A' and num <= 'F')
-//     {
-//         return num - 'A' + 10;
-//     }
-//     return num - 'a' + 10;
-// }
-
-// void memCpyMy(uint8_t* dst, const uint8_t* src, int size)
-// {
-//     // std::cout << size << std::endl;
-//     for (int idx = 0; idx < size; idx++)
-//     {
-//         uint8_t pos1 = charToNum(src[2 * idx]);
-//         uint8_t pos2 = charToNum(src[2 * idx + 1]);
-//         // std::cout << int(dst[size - idx - 1]) << std::endl;
-//         dst[size - idx - 1] = (pos1  << 4) | pos2;
-//         // std::cout << idx << " " << (int)dst[size - idx - 1] << " " <<  (int)pos1 << " " << (int)pos2 << std::endl;
-//     }
-// }
-// void fuzzTarget(const uint8_t *data, size_t size) {
-//     // Ensure there are enough bytes to interpret as a float
-//     if (size * 2 < sizeof(float)) {
-//         std::cout << "Not enough data to interpret as float." << std::endl;
-//         return;
-//     }
-//     for (int idx = 0; idx < size; idx++)
-//     {
-//         // std::cout << idx << " " << std::hex << data[idx] << " " << std::endl;
-//     }
-//     float input_value;
-//     memCpyMy(reinterpret_cast<uint8_t*>(&input_value), data, sizeof(input_value));
-//     // std::cout << "Interpreted float value: " << input_value <<  std::endl;
-//     if (input_value <= 1e-20f and input_value >= -1.0e-20f)
-//     {
-//         // *t  = 1;
-//     }
-//     float result = input_value * input_value;
-//     std::cout << "Squared value: " << result << std::endl;
-// }
-
-// int main(int argc, char **argv) {
-//     // AFL will pass input data as stdin
-
-//     if (argc > 1) {
-//         fuzzTarget(reinterpret_cast<uint8_t*>(argv[1]), strlen(argv[1]));
-//     }
-
-//     return 0;
-// }
-
-#include <cstddef>
+#include <cmath>
 #include <cstdint>
 #include <cstring>
+#include <iostream>
+#include <iomanip>
+#include "math_custom_fun.h"
+#include <fuzzer/FuzzedDataProvider.h>
 
-// This is the function LibFuzzer will call repeatedly
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-    // Example logic: crash if input is "crash"
-    if (size >= 5 && std::memcmp(data, "crash", 5) == 0) {
-        __builtin_trap(); // or abort();
-    }
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+    FuzzedDataProvider fdp(data, size);
+
+    auto value = fdp.ConsumeFloatingPoint<float>();
+
+    // Use the double value
+    std::cout << "Fuzzed double: " << fp32_custom_sine(value) << std::endl;
+
     return 0;
 }
-
